4c14e53aa14853ea9db9433a8857b776b15d68a8
andresfmvaldivieso <andresfmvaldivieso@gmail.com>
2021-12-15 15:27:03 -0500

    coontroller-routes para proyect

--- backend/controllers/proyect.js
+++ backend/controllers/proyect.js
@@ -1,4 +1,25 @@
 import proyect from "../models/proyect.js";
-import fs from "fs";
-import path from "path";
-import moment from "moment";
+
+const saveProyect = async (req, res) => {
+    if (!req.body.name || !req.body.description)
+      return res.status(400).send({ message: "Incomplete data" });
+  
+    const proyectSchema = new proyect({
+      userId: req.user._id,
+      name: req.body.name,
+      description: req.body.description,
+      arrayCollaborators: [],
+    });
+  
+    const result = await proyectSchema.save();
+    return !result
+      ? res.status(400).send({ message: "Error create proyect" })
+      : res.status(200).send({ result });
+  };
+
+  
+  
+  export default {saveProyect,}
+
+
+
