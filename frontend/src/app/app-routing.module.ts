0bc95361984974c17c09b856af801383cfce457c
“NyronFlackters” <cruzsuazocristianandres@gmail.com>
2021-12-18 08:25:10 -0500

    Progresos frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListProyectComponent } from './components/proyect/list-proyect/list-proyect.component';
 import { LayoutComponent } from './home/dashboard/layout/layout.component';
 import { LandingComponent } from './home/landing/landing/landing.component';
 import { LoginComponent } from './home/login/login.component';
@@ -9,7 +10,17 @@ const routes: Routes = [
   { path: '', component: LandingComponent },
   { path:'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  {path: 'dashboard', component: LayoutComponent}
+  {
+    path: 'dashboard',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: ListProyectComponent,
+      },
+    ],
+  },
+  
 ];
 
 @NgModule({
